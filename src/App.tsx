8d57d450990cd00008339ea849f9406a002a3f24
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2023-05-17 11:46:46 +0900

    add remote

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import * as Notifications from 'expo-notifications';
 import { Provider } from 'react-redux';
@@ -6,17 +6,36 @@ import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { MenuProvider } from 'react-native-popup-menu';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import firestore from '@react-native-firebase/firestore';
 
 import { configureStore } from '@/stores/Store';
 
 import RootNavigatorContainer from '@/containers/RootNavigatorContainer';
 import LoadingScreen from '@/screens/LoadingScreen';
 import { AppState, AppStateStatus } from 'react-native';
+import MaintenanceScreen from './screens/MaintenanceScreen';
 
 const { store, persistor } = configureStore();
 
+type ConfigMaintenance = {
+  status: boolean;
+  messageEn: string | null;
+  messageJa: string | null;
+};
+
 const App = () => {
   const appState = useRef<AppStateStatus>(AppState.currentState);
+  const [maintenance, setMaintenance] = useState<ConfigMaintenance>({
+    status: false,
+    messageEn: null,
+    messageJa: null,
+  });
+
+  const getMaintenance = useCallback(async () => {
+    const doc = await firestore().doc('config/maintenance').get();
+    const data = doc.data() as ConfigMaintenance;
+    setMaintenance(data);
+  }, []);
 
   useEffect(() => {
     const subscription = AppState.addEventListener(
@@ -36,10 +55,19 @@ const App = () => {
       appState.current.match(/inactive|background/) &&
       nextAppState === 'active'
     ) {
+      getMaintenance();
       Notifications.setBadgeCountAsync(0);
     }
     appState.current = nextAppState;
   };
+
+  if (maintenance.status)
+    return (
+      <MaintenanceScreen
+        messageEn={maintenance.messageEn}
+        messageJa={maintenance.messageJa}
+      />
+    );
 
   return (
     <Provider store={store}>
