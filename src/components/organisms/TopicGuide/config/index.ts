5bc7c7efe7d64d0df7ff18650dd3a1cb228a28fc
sato-daiki <daiki0520daiki0520@yahoo.co.jp>
2023-06-25 17:24:51 +0900

    fixed warning

--- src/components/organisms/TopicGuide/config/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './selfIntroduction';
-export * from './hobby';
-export * from './job';
-export * from './study';
-export * from './dream';
-export * from './trip';
-export * from './reborn';
